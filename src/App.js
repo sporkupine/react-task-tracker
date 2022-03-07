4aeb8f27fe4a2d83636ebca1383d969711c63d46
sporkupine <coffeechief25@gmail.com>
2022-03-06 22:05:10 -0600

    UI is finished, onto mock backend with JSON server

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,6 +27,13 @@ function App() {
     },
   ]);
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -41,8 +50,11 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
+      <Header 
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+        />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
